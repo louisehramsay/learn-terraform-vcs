7c220535507de69e61b0c9cef54ab77ff7b84f27
root <root@ip-172-31-35-7.eu-west-2.compute.internal>
2025-08-27 16:18:15 +0000

    Remove cloud block

---
+++ terraform.tf
@@ -0,0 +1,23 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+  /*
+  cloud {
+    organization = "louisehramsay_org"
+
+    workspaces {
+      name = "learn-terraform"
+    }
+  }
+*/
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.31.0"
+    }
+  }
+
+  required_version = "~> 1.2"
+}
